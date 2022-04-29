ddcd4b6b3c63e46eaafc0776a32c9dd4921aee27
Андрей Никишов <nickishov99@bk.ru>
2022-04-29 15:47:08 +0300

    Реализован функционал получения из файла информации о студентах

--- Students_Info/Program.cs
+++ Students_Info/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Students_Info
@@ -8,7 +10,18 @@ namespace Students_Info
         //Функция отвечает за вывод информации о студентах
         private static void get_students_info(string _path)
         {
-
+            string[] text;
+            using (StreamReader reader = new StreamReader(_path))
+            {
+                text = reader.ReadToEnd().Split('\n');
+            }
+            Dictionary<string, string> students = new Dictionary<string, string>();
+            foreach(string stroka in text)
+            {
+                students.Add(stroka.Split(" - ")[0], stroka.Split(" - ")[1]);
+            }
+            //TODO: Сортировка студентов по датам рождения
+            //TODO: вывод информации о студентах
         }
         static void Main(string[] args)
         {
