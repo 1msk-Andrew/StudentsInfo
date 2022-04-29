7f4c3eb145b3cfe11c62b3bd6bec818308a3669a
Андрей Никишов <nickishov99@bk.ru>
2022-04-29 15:08:00 +0300

    Пустой проект

--- Students_Info/Program.cs
+++ Students_Info/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Students_Info
 {
@@ -6,7 +7,7 @@ namespace Students_Info
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
